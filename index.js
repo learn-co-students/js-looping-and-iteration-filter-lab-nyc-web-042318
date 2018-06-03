54ec36107b865777a97d00ebed01f8f22d638d11
ensallee <ensallee@gmail.com>
2018-06-03 15:25:26 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(array, string) {
+  return array.filter(function (driver) {return driver.toLowerCase() === string.toLowerCase();});
+}
+
+function fuzzyMatch(array, string) {
+  let lengthOfString = string.length;
+  return array.filter(function (driver) {return driver.slice(0, lengthOfString).toLowerCase() === string.toLowerCase();})
+}
+
+function matchName(array, string) {
+  return array.filter(function (driverObj) {return driverObj.name.toLowerCase() === string.toLowerCase();});
+}
