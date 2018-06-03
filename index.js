c5ec22c9e4aa2b32b029d300b1120af969433d9a
blevm <mjblevins7@gmail.com>
2018-06-03 19:26:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+
+function findMatching(array, name) {
+  const result = array.filter(function (element) { return element.toLowerCase() === name.toLowerCase(); });
+  return result;
+}
+
+function fuzzyMatch(array, letter) {
+  const result = array.filter(function (element) { return element[0] === letter[0]} );
+  return result;
+}
+
+function matchName(object, name) {
+  const result = object.filter(function (key) { return key['name'].toLowerCase() === name.toLowerCase() });
+  return result;
+}
