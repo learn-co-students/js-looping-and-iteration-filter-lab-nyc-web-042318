c463f660fd092211552ef86edbac712e9940099a
Ryan Ghaida <ryaboug@gmail.com>
2018-06-03 21:16:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, str) {
+ return drivers.filter(function(x) { return x.toLowerCase() === str.toLowerCase() });
+};
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(function(x) { return x.slice(0,2) === str });
+};
+
+function matchName(drivers, str) {
+  return drivers.filter(function(x) { return x['name'] === str });
+};
