76b5c6d6eb717175db7acaa147b46c1b252a881e
srajeev91 <github email address>
2018-06-03 21:02:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  return array.filter(function (names) { return names.toLowerCase() === name.toLowerCase(); });
+}
+
+function fuzzyMatch(array, letters) {
+  return array.filter(function (name) { if (name.startsWith(letters)) {return name;} });
+}
+
+function matchName(array, driver) {
+  return array.filter(function (hash) {
+    return hash.name === driver;
+  });
+}
