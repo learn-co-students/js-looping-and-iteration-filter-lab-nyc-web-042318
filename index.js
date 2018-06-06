127b371cb6195330568dbd2ea49905430190985c
github username <github email address>
2018-06-06 18:11:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Code your solution in this file
+//Code your solution in this file
 function findMatching(drivers, string) {
   const people = drivers.filter(driver => driver.toUpperCase() == string.toUpperCase())
   return people;
@@ -13,3 +13,30 @@ function matchName(drivers, string) {
   const people = drivers.filter(drivers => drivers.name == string)
   return people;
 }
+
+
+// Another way to do the Lab
+
+//
+// function findMatching(drivers, string) {
+//  return drivers.filter(function(driver) {return driver.toUpperCase() === string.toUpperCase()});
+// }
+//
+// function fuzzyMatch(drivers, string) {
+//   return drivers.filter(function(driver){
+//     if (driver.slice(0, string.length) === string) {
+//         return driver;
+//       }
+// });
+// }
+//
+// function matchName (drivers, string) {
+//   return drivers.filter(function(driver) {
+//     for (const key in driver) {
+//       if (driver[key] === string) {
+//         return driver
+//       }
+//     }
+//   });
+//
+// }
